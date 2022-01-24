b68af69c654055af22970d807123b6328010ec7f
imran <imranazidy5@gmail.com>
2022-01-25 01:17:47 +0800

    fix item.php

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'item_name',
+        'item_price',
+        'item_available_unit',
+        'catID', // foreign_key
+        'suppID' // foreign_key
+    ];
+
+    // use this to relate items to category
+    // for example, when declaring $item = Item::find(1)
+    // in your view, put this anywhere: {{ $item->category->cat_name }}
+    // or use $item->category->id also can :)
+    public function category() {
+        return $this->belongsTo(Category::class, 'catID');
+    }
+
+    // same story for supplier function
+    // use something like this who owns the item:
+    // {{ $item->supplier->name }}
+    public function supplier() {
+        return $this->belongsTo(User::class, 'suppID');
+    }
+
+    // to retrieve all purchase details that was ordered by customers
+    // use: {{ $item->purchases }}
+    public function purchases() {
+        return $this->hasMany(Purchase::class, 'itemID');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
         'name',
@@ -30,7 +30,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
@@ -40,7 +40,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be cast.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
